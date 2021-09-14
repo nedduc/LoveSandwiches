38930fdd9819d5b0c3578c54c2c3a41a0e92a293
nedduc <marinacudden@gmail.com>
2021-09-14 17:10:12 +0000

    Add user request loop to repeat request until data provided is valid

--- run.py
+++ run.py
@@ -17,17 +17,19 @@ def get_sales_data():
     """
     get sales data input from user
     """
+    while True:
+        print("please enter sales data from the last market.")
+        print("Data should be six numbers, sepeartaed by comas.")
+        print("Example: 10,20,30,40,50,60\n")
 
+        data_str = input("Enter your data here: ")
 
-    print("please enter sales data from the last market.")
-    print("Data should be six numbers, sepeartaed by comas.")
-    print("Example: 10,20,30,40,50,60\n")
-
-    data_str = input("Enter your data here: ")
-
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
-
+        sales_data = data_str.split(",")
+     
+        if validate_data(sales_data):
+            print("Data is valid")
+            break
+retuen sales_data
 
 def validate_data(values):
     """ Inside the try, converts all string values into intergers.
@@ -35,13 +37,29 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provide {len(values)}"
             )
     except ValueError as e:
-                print(f"Invalid data: {e}, please try again.\n")
-    print(values)
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 
-get_sales_data()
+def update_sales_worksheet(data):
+    """
+    Update sales workshette, add new row with the list data provided.
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
+print(data)
